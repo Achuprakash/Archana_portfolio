dd2e1534812b66cbdab82027a81fbd095e786aac
Achuprakash <jarchu54@gmail.com>
2025-09-26 22:40:18 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace 'Archana_portfolio' with your actual repository name
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'https://github.com/Achuprakash/Archana_portfolio, // <-- ADD OR EDIT THIS LINE
+});
